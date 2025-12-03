f4531839245d5f3a91422cae02887ad45710a9ed
unknown <talonpopy@gmail.com>
2025-12-03 22:33:15 +0700

    Lab_1.2

---
+++ Lab_1.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void GetSet(int data[], int *num ) {
+    printf("Enter member : ");
+    scanf("%d",num);
+
+    for (int i = 1; i <= *num; i++)
+    {
+        printf("Enter your data : ", i+1);
+        scanf("%d",&data[i]);
+    }
+    
+}
+int main(){
+    int data[100], num;
+    GetSet(data, &num) ;
+    printf("Member : %d\n",num);
+    for (int i = 1; i <= num; i++)
+    {
+        printf("data[%d] : %d\n",i,data[i]);
+    }  
+    return 0;
+}
